1231421247e87c3fc34b1731a7b3cb31031d2bbb
Kimberly Yi <yikim01@gmail.com>
2015-07-29 17:14:55 -0700

    index cleanup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
+  end
+
+  def show
   end
 
   def new
@@ -9,8 +13,31 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed. Please check the form."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
@@ -19,4 +46,7 @@ class PostsController < ApplicationController
     params.require(:post).permit(:image, :caption)
   end
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
